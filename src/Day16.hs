a7c16bcb749e6a2ac77f7a47abec63560d42f9d4
Will Mitchell <will.mitchell@assemble.inc>
2021-12-16 14:31:01 -0500

    Clean up using State monad

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,19 +1,25 @@
 module Day16 (part16a, part16b) where
-import           Data.Foldable (Foldable (foldl'))
-import           Debug.Trace
-import           Util          (readLines)
+
+import Control.Monad.State (MonadState (get), State, evalState, get, put, runState)
+import Data.Foldable (Foldable (foldl'))
+import Debug.Trace
+import Util (readLines)
 
 --------------------------------------------------------------------------------
--- Day 16 -
+-- Day 16 - Packet Decoder
 --------------------------------------------------------------------------------
 data Bit = Zero | One deriving (Eq, Show, Enum)
 
-data Payload = Literal { typeID :: Int, value :: Int }
-             | Operator { typeID :: Int, subpackets :: [Packet] } deriving (Eq, Show)
+data Payload
+  = Literal {typeID :: Int, value :: Int}
+  | Operator {typeID :: Int, subpackets :: [Packet]}
+  deriving (Eq, Show)
 
-data Packet = Packet { version :: Int
-                     , payload :: Payload
-                     } deriving (Eq, Show)
+data Packet = Packet
+  { version :: Int,
+    payload :: Payload
+  }
+  deriving (Eq, Show)
 
 hex2Bits :: String -> [Bit]
 hex2Bits = concatMap parseBit
@@ -34,83 +40,107 @@ hex2Bits = concatMap parseBit
     parseBit 'D' = [One, One, Zero, One]
     parseBit 'E' = [One, One, One, Zero]
     parseBit 'F' = [One, One, One, One]
-    parseBit _   = error "Invalid hex character"
+    parseBit _ = error "Invalid hex character"
 
 bin2Dec :: [Bit] -> Int
 bin2Dec = foldl' (\acc x -> acc * 2 + fromEnum x) 0
 
-parseValue :: [Bit] -> (Int, [Bit])
-parseValue bits = (bin2Dec unchunked, rest')
+popBits :: Int -> State [Bit] [Bit]
+popBits n = do
+  bits <- get
+  let (result, rest) = splitAt n bits
+  put rest
+  return result
+
+popInt :: Int -> State [Bit] Int
+popInt n = bin2Dec <$> popBits n
+
+popBit :: State [Bit] Bit
+popBit = do
+  bits <- get
+  put $ tail bits
+  return $ head bits
+
+parseValue :: State [Bit] Int
+parseValue =
+  let chunk = do
+        continue <- popBit
+        quad <- popBits 4
+        if continue == One
+          then (quad ++) <$> chunk
+          else return quad
+   in bin2Dec <$> chunk
+
+parseNPackets :: Int -> State [Bit] [Packet]
+parseNPackets 0 = return []
+parseNPackets n = do
+  packet <- parsePacket
+  packets <- parseNPackets (n - 1)
+  return (packet : packets)
+
+parseBoundedPackets :: Int -> State [Bit] [Packet]
+parseBoundedPackets 0 = return []
+parseBoundedPackets l = do
+  before <- get
+  packet <- parsePacket
+  after <- get
+  packets <- parseBoundedPackets (l - (length before - length after))
+  return (packet : packets)
+
+parseSubpackets :: State [Bit] [Packet]
+parseSubpackets = do
+  ltype <- popBit
+  if ltype == Zero
+    then do
+      length <- popInt 15
+      parseBoundedPackets length
+    else do
+      length <- popInt 11
+      parseNPackets length
+
+parsePayload :: State [Bit] Payload
+parsePayload = do
+  typeID <- popInt 3
+  if typeID == 4
+    then do
+      Literal typeID <$> parseValue
+    else do
+      Operator typeID <$> parseSubpackets
+
+parsePacket :: State [Bit] Packet
+parsePacket = do
+  version <- popInt 3
+  Packet version <$> parsePayload
+
+sumVersions :: Packet -> Int
+sumVersions Packet {version = v, payload = p} = v + sumVersions' p
   where
-    (unchunked, rest') = chunk bits
-    chunk (One:a:b:c:d:rest) =
-      let (cs, rest') = chunk rest
-      in (a:b:c:d:cs, rest')
-    chunk (Zero:a:b:c:d:rest) = ([a, b, c, d], rest)
-    chunk _ = error "Invalid value"
-
-parseSubpackets :: [Bit] -> ([Packet], [Bit])
-parseSubpackets (Zero:rest) =
-  let subPacketLength = bin2Dec $ take 15 rest
-      afterLength = drop 15 rest
-      foldPacket 0 ps bits = (ps, bits)
-      foldPacket n ps bits =
-        let (p, rest') = parsePacket bits
-        in foldPacket (n - (length bits - length rest')) (ps ++ [p]) rest'
-  in foldPacket subPacketLength [] afterLength
-parseSubpackets (One:rest) =
-  let subPacketLength = bin2Dec $ take 11 rest
-      afterLength = drop 11 rest
-      foldPacket 0 ps bits = (ps, bits)
-      foldPacket n ps bits =
-        let (p, rest') = parsePacket bits
-        in foldPacket (n-1) (ps ++ [p]) rest'
-  in foldPacket subPacketLength [] afterLength
-parseSubpackets _ = error "Invalid subpacket"
-
-parsePayload :: [Bit] -> (Payload, [Bit])
-parsePayload (One:Zero:Zero:rest) = (Literal { typeID = 4, value = value }, rest')
-  where (value, rest') = parseValue rest
-parsePayload (t1:t2:t3:rest) = (Operator { typeID = bin2Dec [t1, t2, t3], subpackets = subpackets }, rest')
-  where
-    (subpackets, rest') = parseSubpackets rest
-parsePayload rs = (Literal { typeID = 4, value = -99 }, rs)
-
-parsePacket :: [Bit] -> (Packet, [Bit])
-parsePacket (v1:v2:v3:rest) =
-  let (payload, rest') = parsePayload rest
-  in (Packet { version = bin2Dec [v1, v2, v3]
-             , payload = payload
-             }, rest')
-parsePacket _ = error "Invalid packet"
-
-addAllVersionNumbers :: Packet -> Int
-addAllVersionNumbers Packet { version = v, payload = Literal {} } = v
-addAllVersionNumbers Packet { version = v, payload = Operator { typeID = t, subpackets = ps } } = v + sum (map addAllVersionNumbers ps)
+    sumVersions' (Literal _ _) = 0
+    sumVersions' (Operator _ ps) = sum (map sumVersions ps)
 
 valueOf :: Packet -> Int
-valueOf Packet { payload = Literal { value = v' } } = v'
-valueOf Packet { payload = Operator { typeID = t, subpackets = ps } } =
+valueOf Packet {payload = Literal {value = v'}} = v'
+valueOf Packet {payload = Operator {typeID = t, subpackets = ps}} =
   let subValues = map valueOf ps
-  in case t of
-    0 -> sum subValues
-    1 -> product subValues
-    2 -> minimum subValues
-    3 -> maximum subValues
-    -- type 4 is literal
-    5 -> fromEnum (head subValues > (head . tail) subValues)
-    6 -> fromEnum (head subValues < (head . tail) subValues)
-    7 -> fromEnum (head subValues == (head . tail) subValues)
-    _ -> 0
+   in case t of
+        0 -> sum subValues
+        1 -> product subValues
+        2 -> minimum subValues
+        3 -> maximum subValues
+        -- type 4 is literal
+        5 -> fromEnum (head subValues > (head . tail) subValues)
+        6 -> fromEnum (head subValues < (head . tail) subValues)
+        7 -> fromEnum (head subValues == (head . tail) subValues)
+        _ -> error ("Unknown operator: " ++ show t)
 
 part16a :: IO Int
 part16a = do
   input <- head <$> readLines "inputs/day16.txt"
   let packet = hex2Bits input
-  return $ addAllVersionNumbers $ fst $ parsePacket packet
+  return $ sumVersions $ evalState parsePacket packet
 
 part16b :: IO Int
 part16b = do
   input <- head <$> readLines "inputs/day16.txt"
   let packet = hex2Bits input
-  return $ valueOf $ fst $ parsePacket packet
+  return $ valueOf $ evalState parsePacket packet
