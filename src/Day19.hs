6d881c7fc681be9655fdc372f4c94a6a370300eb
Will Mitchell <w@wakamoleguy.com>
2021-12-19 11:11:42 -0500

    Day 19 - Beacon Scanner

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,21 +1,85 @@
 module Day19 (part19a, part19b) where
-
+import           Data.List.Split (splitWhen)
+import           Data.Maybe      (catMaybes, listToMaybe)
+import qualified Data.Set        as Set
+import           Util            (readCommaSeparatedInts, readLines)
 --------------------------------------------------------------------------------
 -- Day 19 - Beacon Scanner
 --------------------------------------------------------------------------------
 type Coord = (Int, Int, Int)
+type CoordSet = Set.Set Coord
 
-rotations :: Coord -> [Coord]
-rotations (x, y, z) = [ (x, y, z), (x, -z, y), (x, -y, -z), (x, z, -y)
-                      , (-x, -y, z), (-x, -z, -y), (-x, y, -z), (-x, z, y)]
-turns :: Coord -> [Coord]
-turns (x, y, z) = [ (x, y, z), (z, x, y), (y, z, x) ]
+-- Input parsing
+input :: IO [String]
+input = readLines "inputs/day19.txt"
 
-perspectives :: Coord -> [Coord]
-perspectives c = pure c >>= rotations >>= turns
+listToCoord :: [Int] -> Coord
+listToCoord [x, y, z] = (x, y, z)
+listToCoord _         = error "listToCoord: invalid input"
 
-part19a :: IO String
-part19a = pure $ show $ perspectives (1, 2, 3)
+parsedScannerCoords :: [String] -> [CoordSet]
+parsedScannerCoords = map parseSingleScanner . splitWhen (== "")
+  where
+    parseSingleScanner = Set.fromList . map (listToCoord . readCommaSeparatedInts) . tail
 
-part19b :: IO String
-part19b = pure "World"
+-- Generate rotations and perspectives
+rotations :: [Coord -> Coord]
+rotations = [ \(x, y, z) -> (x, y, z)
+            , \(x, y, z) -> (x, -z, y)
+            , \(x, y, z) -> (x, -y, -z)
+            , \(x, y, z) -> (x, z, -y)
+            , \(x, y, z) -> (-x, -y, z)
+            , \(x, y, z) -> (-x, -z, -y)
+            , \(x, y, z) -> (-x, y, -z)
+            , \(x, y, z) -> (-x, z, y) ]
+
+turns :: [Coord -> Coord]
+turns = [ \(x, y, z) -> (x, y, z)
+        , \(x, y, z) -> (z, x, y)
+        , \(x, y, z) -> (y, z, x) ]
+
+perspectives :: [Coord -> Coord]
+perspectives = (.) <$> turns <*> rotations
+
+-- Rotate the first set until it overlaps with the second, then return the result
+-- If not overlap is found, return Nothing
+orientTo :: CoordSet -> ((Int, Int, Int), CoordSet) -> Maybe ((Int, Int, Int), CoordSet)
+orientTo unoriented ((dtx, dty, dtz), target) = listToMaybe $ catMaybes $ do
+  perspective <- perspectives
+  let rotated = Set.map perspective unoriented
+  (tdx, tdy, tdz) <- Set.toList target
+  (rdx, rdy, rdz) <- Set.toList rotated
+  let (dx, dy, dz) = (rdx - tdx, rdy - tdy, rdz - tdz)
+  let shiftedRotated = Set.map (\(x, y, z) -> (x - dx, y - dy, z - dz)) rotated
+  let sizeOfIntersection = Set.size $ Set.intersection shiftedRotated target
+  if sizeOfIntersection >= 12
+    then return (Just ((-dx, -dy, -dz), shiftedRotated))
+    else return Nothing
+
+orientToAny :: CoordSet -> [((Int, Int, Int), CoordSet)] -> Maybe ((Int, Int, Int), CoordSet)
+orientToAny unoriented targets = listToMaybe $ catMaybes $ orientTo unoriented <$> targets
+
+orientAll :: [((Int, Int, Int), CoordSet)] -> [CoordSet] -> [((Int, Int, Int), CoordSet)]
+orientAll oriented [] = oriented
+orientAll oriented unoriented =
+  let next = head unoriented
+  in case orientToAny next oriented of
+    Nothing           -> orientAll oriented (tail unoriented ++ [next]) -- Try later
+    Just nextOriented -> orientAll (nextOriented : oriented) (tail unoriented)
+
+
+mappedScanners :: IO [((Int, Int, Int), CoordSet)]
+mappedScanners = do
+  scanners <- parsedScannerCoords <$> input
+  return $ orientAll [((0, 0, 0), head scanners)] (tail scanners)
+
+part19a :: IO Int
+part19a = Set.size . Set.unions . map snd <$> mappedScanners
+
+manhattanDistance :: Coord -> Coord -> Int
+manhattanDistance (x1, y1, z1) (x2, y2, z2) = abs (x1 - x2) + abs (y1 - y2) + abs (z1 - z2)
+
+part19b :: IO Int
+part19b = do
+  scanners <- map fst <$> mappedScanners
+  return $ maximum [manhattanDistance p1 p2 | p1 <- scanners, p2 <- scanners, p1 /= p2]
--- test/Spec.hs
+++ test/Spec.hs
@@ -23,9 +23,9 @@ main :: IO ()
 main = hspec $ do
   describe "Day 19" $ do
     it "solves day19a" $ do
-      part19a >>= (`shouldBe` [])
+      part19a >>= (`shouldBe` 376)
     it "solves day19b" $ do
-      part19b >>= (`shouldBe` [])
+      part19b >>= (`shouldBe` 10772)
 
   describe "Day 18" $ do
     it "solves day18a" $ do
